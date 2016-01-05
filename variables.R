5776f4d207176e84e3ea8bb6970b6a89f4776f2f
Rebecca Meyer <rebeccameyer2159@gmail.com>
2016-01-05 11:26:28 -0500

    adding commented header_firstcommit

---
+++ variables.R
@@ -0,0 +1,16 @@
+#This is the document where I learn introductory R
+#I did not save my variables.R file yesterday whoops
+#This is my new document to make changes to for github practice.
+#Here are some random things:
+
+install.packages("ggplot2")
+install.packages("gapminder")
+x <- 1L #force to be an integer instead of a double
+typeof(x)
+y <- as.integer(2)
+typeof(y)
+
+x <- 1:4
+names(x) <- c("a", "b", "c", "d")
+typeof(x)
+
